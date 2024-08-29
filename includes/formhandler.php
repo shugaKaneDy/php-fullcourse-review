a22f62485fe58d9697a6973cd662c69727e41f7f
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-08-29 22:54:58 +0800

    PHP review 1

---
+++ includes/formhandler.php
@@ -0,0 +1,6 @@
+<?php
+
+// echo htmlspecialchars($_POST['chars']);
+// $chars = $_POST['chars'];
+// $chars = htmlspecialchars($_POST['chars']);
+// echo $chars;
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-  <h1><?= "Hello World" ?></h1>
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+
+// GET = used to show the data from database
+// POST = used to submit data to database
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// } = used this. this is the best way to do it
+// Sanitize data that you submit. Don't trust user
+// always use htmlspecialchars($_POST["fistname"]) for form;
+
+
+/* $a = 1;
+$result = match() {
+  1,3,5 => "Variable a is equal to one",
+  2 => "Variable a is equal to two",
+  default => "None were match",
+};
+ */
+
+
+echo "Hello World!";
